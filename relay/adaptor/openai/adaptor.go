3405d33333866301e7442befd3659e6b0b77c762
liuliming <liuliming@yupaopao.cn>
2024-05-11 00:15:30 +0800

    add log

--- relay/adaptor/openai/adaptor.go
+++ relay/adaptor/openai/adaptor.go
@@ -1,9 +1,11 @@
 package openai
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/songquanpeng/one-api/common/logger"
 	"github.com/songquanpeng/one-api/relay/adaptor"
 	"github.com/songquanpeng/one-api/relay/adaptor/minimax"
 	"github.com/songquanpeng/one-api/relay/channeltype"
@@ -90,6 +92,7 @@ func (a *Adaptor) DoResponse(c *gin.Context, resp *http.Response, meta *meta.Met
 			usage = ResponseText2Usage(responseText, meta.ActualModelName, meta.PromptTokens)
 		}
 		if usage.TotalTokens != 0 && usage.PromptTokens == 0 { // some channels don't return prompt tokens & completion tokens
+			logger.Info(context.Background(), fmt.Sprintf("Usage tokens maybe abnormal, response=%s, meta=%v", responseText, *meta))
 			usage.PromptTokens = meta.PromptTokens
 			usage.CompletionTokens = usage.TotalTokens - meta.PromptTokens
 		}
